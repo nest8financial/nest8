3bcf33f0a4f8047b5d54fa81c887a7fd67a51c06
Kristin Huot <kristin.huot@gmail.com>
2024-06-17 13:48:13 -0500

    passing month and year, set up functions for buttons

--- src/components/InputHeader/InputHeader.jsx
+++ src/components/InputHeader/InputHeader.jsx
@@ -1,14 +1,30 @@
-import * as React from 'react';
 import dayjs from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { useState } from 'react';
 import { Typography } from '@mui/material';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 function InputHeader(){
 
+const history = useHistory(); 
 const [date, setDate] = useState(dayjs()); 
+
+const addInput = (date) => {
+    history.push({
+        pathname: 'add_input',
+        state: {month: date.format('MM'), year: date.format('YYYY')}
+    })
+}
+
+const editInput = (date) => {
+    history.push({
+        pathname: 'edit_input',
+        state: {month: date.format('MM'), year: date.format('YYYY')}
+
+    })
+}
 
 
 return (
@@ -17,11 +33,12 @@ return (
           <DateCalendar 
           value={date} onChange={(newValue) => setDate(newValue)} 
           views={['month', 'year']}
+          openTo='month'
           />
         <Typography>{date.format('MMM YYYY')}</Typography>
         <Container>
-            <Button>Add Input</Button>
-            <Button>Edit Input</Button>
+            <Button onClick={addInput(date)}>Add Input</Button>
+            <Button onClick={editInput(date)}>Edit Input</Button>
         </Container>
       </Container>
     </LocalizationProvider>
