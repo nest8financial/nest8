580e4bbaeb2d26be3f63ec60b7310066a09256cd
Asia Osman <asiaosman06@gmail.com>
2024-06-18 10:46:09 -0500

    added a placeholder for my select input

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,60 +1,62 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Container, TextField, Button, Typography } from '@mui/material';
-import { useEffect } from 'react';
-import { Select, MenuItem } from '@mui/material';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Container, TextField, Button, Typography } from "@mui/material";
+import { useEffect } from "react";
+import { Select, MenuItem } from "@mui/material";
 
 function RegisterForm() {
-const industries = useSelector(store => store.industries)
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [companyName, setCompanyName] = useState('');
-  const [industry, setIndustry] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const industries = useSelector((store) => store.industries);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [industry, setIndustry] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
 
-useEffect(() => {
-dispatch({
-type: 'FETCH_INDUSTRIES'
-}
-)
-},[])
+  useEffect(() => {
+    dispatch({
+      type: "FETCH_INDUSTRIES",
+    });
+  }, []);
 
   const registerUser = (event) => {
     event.preventDefault();
 
     dispatch({
-      type: 'REGISTER',
+      type: "REGISTER",
       payload: {
         firstName: firstName,
         lastName: lastName,
-        companyName:companyName,
-        industry:industry,
-        email:email,
-        password:password,
-        confirmPassword:confirmPassword
+        companyName: companyName,
+        industry: industry,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
       },
     });
   }; // end registerUser
 
   return (
     <Container maxWidth="sm">
-     {/* Hi {JSON.stringify(industries )} */}
-      <form onSubmit={registerUser} style={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Account registration
+      {/* Hi {JSON.stringify(industries )} */}
+      <form
+        onSubmit={registerUser}
+        style={{ display: "flex", flexDirection: "column", gap: 2 }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          Account registration
         </Typography>
 
-      {errors.registrationMessage && (
-        <Typography variant="body2" color="error">
-          {errors.registrationMessage}
-        </Typography>
-      )}
-       <img alt="Login Icon" src="favicon.ico" />
-     <TextField
+        {errors.registrationMessage && (
+          <Typography variant="body2" color="error">
+            {errors.registrationMessage}
+          </Typography>
+        )}
+        <img alt="Login Icon" src="favicon.ico" />
+        <TextField
           label="First Name"
           type="text"
           value={firstName}
@@ -77,18 +79,23 @@ type: 'FETCH_INDUSTRIES'
           onChange={(event) => setCompanyName(event.target.value)}
           fullWidth
         />
-              <Select
-        labelId="dynamic-select-label"
-        value={industry}
-        onChange={(event) => setIndustry(event.target.value)}
-        label="Select an Option">
-          console.log('Industries',industries)
-        {industries?.map((industry) => (
-          <MenuItem key={industry.id} value={industry.id}>
-            {industry.name}
+        <Select
+          labelId="dynamic-select-label"
+          value={industry}
+          onChange={(event) => setIndustry(event.target.value)}
+          displayEmpty
+          label="Industry"
+        >
+          <MenuItem value="" disabled>
+            Select an Industry
           </MenuItem>
-        ))}
-      </Select>
+          {industries?.map((industry) => (
+            <MenuItem key={industry.id} value={industry.id}>
+              {industry.name}
+            </MenuItem>
+          ))}
+        </Select>
+
         <TextField
           label="Email"
           type="email"
