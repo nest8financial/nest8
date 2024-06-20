37298b9e3da1cc1cccbbb7ea920f9ee88481aefe
Kristin Huot <kristin.huot@gmail.com>
2024-06-20 15:41:16 -0500

    un-installed moment and installed date-fns

--- src/components/MyData/MyData.jsx
+++ src/components/MyData/MyData.jsx
@@ -1,80 +1,93 @@
-import { Button, Box, Container, Typography, Link } from "@mui/material";
+import {
+  Button,
+  Box,
+  Container,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+} from "@mui/material";
 import { useSelector } from "react-redux";
-import moment from 'moment'
+import { differenceInMonths, startOfMonth, endOfMonth, parse, format, parseISO } from 'date-fns';
+
+function MyData() {
+  const userData = useSelector((store) => store.user); // pulls the user data from the user store
+  const monthlyInputs = useSelector((store) => store.financialInputs); // pulls the monthly inputs for a user from the financialInputs store
+
+  console.log("this is the user data", userData);
+  console.log("these are the monthly inputs", monthlyInputs);
+
+  const today = new Date(); // pulls the current date
+  const currentMonthYear = format(today, 'MM, yyyy') // reformats to provide just the month and year 
+
+//   console.log('this is todays month', currentMonthYear);
+
+  const parsedJoinDate = parseISO(userData.date_joined) // parses the date joined frmo the userData object
+  const joinDate = format(parsedJoinDate, 'MM, yyyy') // reformats to provide just the month and year 
+
+//   console.log('this is the parsed date', parsedJoinDate);
+//   console.log('this is the join date', joinDate);
+
+const calculateMissingMonths = (startDate, endDate) => {
 
 
-
-function MyData(){
-
-const userData = useSelector(store=> store.user) // pulls the user data from the user store 
-const monthlyInputs = useSelector(store=> store.financialInputs) // pulls the monthly inputs for a user from the financialInputs store
-
-console.log('this is the user data', userData);
-console.log('these are the monthly inputs', monthlyInputs);
-
-const today = moment(); // pulls the current date
-const currentMonthYear = today.format('MM, YYYY') // reformats the current date to be the month and year
-const joinDate = moment(userData.date_joined) // pulls the join date from the User store for the logged in user
-const joinMonthYear = joinDate.format('MM, YYYY'); // reformats the join date to the be the month and year 
-
-console.log('this is the current month and year', currentMonthYear);
-console.log('this is the join date, month and year', joinMonthYear);
-
-const calculateMissingMonths = (start, current) => {
-    let missingMonths = []; // initializes a missingMonths counter to an empty array 
-    let startMonth = start.clone(); // creates a startMonth variable by cloning the start date.
-
-    while (startMonth.isSameOrBefore(current)) {
-        // Check if the current month is missing in the database. If the month is missing, we execute the code inside the if block
-        const monthStr = startMonth.format('MM, YYYY');
-        const isMonthMissing = !checkStoreForMonth(monthStr);
-  
-        if (isMonthMissing) { 
-            missingMonths.push(monthStr); // Pushing the missing month string to the missingMonths array
-        }
-  
-        startMonth.add(1, 'month'); // 
-      }
-  
-      return missingMonths;
 
 }
 
-const checkStoreForMonth = (monthStr) => {
-    return monthlyInputs.some(input => {
-      const inputMonthYear = moment(`${input.year}-${input.month}-01`).format('MM, YYYY');
-      return inputMonthYear === monthStr;
-    });
-  };
+const missingMonths = []; 
 
-const missingMonthsCount = calculateMissingMonths(joinMonthYear, currentMonthYear);
-
-
-    return(
-
-      <Container>
-        <Box>
-            <Button variant="contained" sx={{ mb: 2, mr: 2}}>My Reports</Button>
-        </Box>
-        <Box>
-            <Button variant="contained" sx={{ mb: 2, mr: 2}}>My Profile</Button>
-        </Box>
-        <Box>
-            <Button variant="contained" sx={{ mb: 2, mr: 2}}>My Saved Inputs</Button>
-        </Box>
-        <Box>
-        <Typography variant="h2" fontSize={32}>Alerts</Typography>
-        <List></List>
-        
-        </Box>
-
-
-      </Container>
-
-
-    )
-
+  return (
+    <Container>
+      <Box>
+        <Button variant="contained" sx={{ mb: 2, mr: 2 }}>
+          My Reports
+        </Button>
+      </Box>
+      <Box>
+        <Button variant="contained" sx={{ mb: 2, mr: 2 }}>
+          My Profile
+        </Button>
+      </Box>
+      <Box>
+        <Button variant="contained" sx={{ mb: 2, mr: 2 }}>
+          My Saved Inputs
+        </Button>
+      </Box>
+      <Box>
+        <Typography variant="h2" fontSize={32}>
+          Alerts
+        </Typography>
+        {missingMonths.length > 0 && (
+          <>
+            <List>
+              {missingMonths.map((month) => (
+                <ListItem key={month}>
+                  <Paper>
+                    <Box>
+                        <ListItemText
+                        primary={
+                            <Typography variant="subtitle1" fontWeight="bold">
+                            {month}
+                            </Typography>
+                        }
+                        secondary={
+                            <Typography variant="body2" color="error">
+                            You have uncompleted recommendation action items for{month}
+                            {month}
+                            </Typography>
+                        }
+                        />
+                    </Box>
+                  </Paper>
+                </ListItem>
+              ))}
+            </List>
+          </>
+        )}
+      </Box>
+    </Container>
+  );
 }
 
-
-export default MyData; 
+export default MyData;
