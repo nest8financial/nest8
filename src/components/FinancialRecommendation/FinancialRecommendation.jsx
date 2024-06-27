a8a4f3a61175ad2b0ef597e135dd9060851041b6
Lisa Donohoo <lisa@Alisas-MBP-2.localdomain>
2024-06-27 15:44:27 -0500

    added ai recommendations to rec summary page

--- src/components/FinancialRecommendation/FinancialRecommendation.jsx
+++ src/components/FinancialRecommendation/FinancialRecommendation.jsx
@@ -7,17 +7,32 @@ import { Paper,
          CardActions, 
          Typography, 
          Button,
-         Box }  from '@mui/material';
-import { getMonthName } from '../../utilities/utilities.js'
+         Box,
+         Accordion,
+         AccordionDetails,
+         AccordionSummary }  from '@mui/material';
+         import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+         import { getMonthName } from '../../utilities/utilities.js'
+         import { styled } from '@mui/system';
 
 function FinancialRecommendation({month, year, company}){
 
 console.log('month',month);
 console.log('year',year);
     const recommendations =  useSelector(store => store.financialMetrics.singleMonthMetrics);
-    
     const dispatch = useDispatch();
 
+    const CustomAccordionSummary = styled(AccordionSummary)(({ theme }) => ({
+        flexDirection: 'column',
+        alignItems: 'center',
+        '& .MuiAccordionSummary-content': {
+          order: 1,
+        },
+        '& .MuiAccordionSummary-expandIconWrapper': {
+          order: 2,
+          marginTop: theme.spacing(1),
+        },
+      }));
 
     useEffect(() => {
         if (month && year) {
@@ -28,6 +43,7 @@ console.log('year',year);
         });
         console.log("month",month);
     }
+
        }, [dispatch, month, year])
         console.log("Recommendations updated:", recommendations);
  
@@ -38,53 +54,74 @@ console.log('year',year);
         <Divider sx={{ my: 2 }} textAlign="left" >RECOMMENDATIONS</Divider>
         <br></br>
         <Paper elevation={10}>
-            <br></br>
-            <Typography variant="h5"
-                align="center"
-                sx={{m: 2}}>Recommendations for</Typography>
-                <Typography variant="h4" align="center"> {company}</Typography>
-            <Typography variant="h5" align="center" sx={{m: 2}}>{getMonthName(month)} {year}
-            </Typography>
-            {recommendations.length === 0 ?
-                (
+                <br></br>
+                <Typography variant="h5" align="center" sx={{ m: 2 }}>Recommendations for</Typography>
+                <Typography variant="h4" align="center">{company}</Typography>
+                <Typography variant="h5" align="center" sx={{ m: 2 }}>{getMonthName(month)} {year}</Typography>
+                {recommendations.length === 0 ? (
                     <Box>
-                        <Typography sx={{ color: 'red',
-                                        fontStyle: 'italic',
-                                        textAlign: 'center',
-                                        p: 2 }}
-                                    variant="body1"
-                        >Please provide inputs for this month to see Summary information
+                        <Typography sx={{ color: 'red', fontStyle: 'italic', textAlign: 'center', p: 2 }} variant="body1">
+                            Please provide inputs for this month to see Summary information
                         </Typography>
-                        </Box>
+                    </Box>
                 ) : (
                     <>
-                        {recommendations?.map((recommendation) => (
-                        <Card key={recommendation.id}
-                            sx={{m: 1,
-                                backgroundColor: (recommendation.variance_value >= 0 ? 
-                                    'rgba(226,242,242,100)' : 'rgba(246,195,191,100)') }}>
-                            <CardContent sx={{ p: 3 }}>
-                                <Typography variant="h6">{recommendation.metric_name}</Typography>
-                                <Typography variant="body1"
-                                >{`${recommendation.variance_value >= 0 ?
-                                    recommendation.recommendation_positive_text : 
-                                    recommendation.recommendation_negative_text}`}
-                                </Typography>
-                            </CardContent>
-                            <CardActions sx={{mt: -3, mb: -1 }}>
-                                <Button fullWidth={true}
-                                        size="small"
-                                        sx={{ justifyContent: 'left', pl: 2 }} 
-                                        >DETAILS</Button>
-                            </CardActions>
-                        </Card>
-                        
-                        ))}
+                        {recommendations.map((recommendation) => {
+                            const positiveParts = recommendation.recommendation_positive_text.split('*');
+                            const negativeParts = recommendation.recommendation_negative_text.split('*');
+
+                            const positiveText0 = positiveParts[0] ? positiveParts[0].replace('<User>', company) : '';
+                            const positiveText1 = positiveParts[1] ? positiveParts[1].replace('<User>', company) : '';
+                            const negativeText0 = negativeParts[0] ? negativeParts[0].replace('<User>', company) : '';
+                            const negativeText1 = negativeParts[1] ? negativeParts[1].replace('<User>', company) : '';
+
+                            return (
+                                <Card key={recommendation.id}
+                                    sx={{
+                                        m: 1,
+                                        backgroundColor: (recommendation.variance_value >= 0 ?
+                                            'rgba(226,242,242,100)' : 'rgba(246,195,191,100)')
+                                    }}>
+                                    <Typography variant="h6">{recommendation.metric_name}</Typography>
+                                    <Accordion>
+                                        <CustomAccordionSummary expandIcon={<ExpandMoreIcon />}
+                                                                aria-controls="panel1-content"
+                                                                id="panel1-header">
+                                            <Box sx={{ display: 'flex', 
+                                                       flexDirection: 'column', 
+                                                       textAlign: 'center', 
+                                                       mb: -3
+                                                }}>
+                                                <Typography variant="body1">
+                                                        {recommendation.variance_value >= 0 ? (
+                                                            <>
+                                                                {positiveText0}
+                                                                <br /><br />
+                                                                {positiveText1}
+                                                            </>
+                                                        ) : (
+                                                            <>
+                                                                {negativeText0}
+                                                                <br /><br />
+                                                                {negativeText1}
+                                                            </>
+                                                        )}
+                                                    </Typography>
+                                                    <Typography variant="body1" 
+                                                                sx={{ fontWeight: 'bold', fontSize: '0.875rem', pt: 1.5 }}>MORE INFO</Typography>
+                                                </Box>
+                                        </CustomAccordionSummary>
+                                        <AccordionDetails>
+                                            {recommendation.recommendation_ai_enhanced}
+                                        </AccordionDetails>
+                                    </Accordion>
+                                </Card>
+                            );
+                        })}
                     </>
                 )}
                 <br></br>
-            
-        </Paper>
+            </Paper>
         <br></br>
         </>
     
